581bde167ff3c85c71e1930b12a395026e252f07
Dianne <dianne.vincent@stonybrook.edu>
2024-12-02 15:13:51 -0500

    Portfolio Comitted

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,186 +20,129 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
-  subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
-  ),
-  resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
-  displayGreeting: true // Set false to hide this section, defaults to true
-};
+    username: "Dianne Vincent",
+    title: "Hi all, I'm Dianne",
+    subTitle: emoji(
+      "A passionate Computer Science student actively seeking opportunities in Software Engineering and Machine Learning, with experience in developing decentralized applications, full-stack web apps, and research in bias mitigation for AI models."
+    ),
+
+    displayGreeting: true // Set false to hide this section, defaults to true
+  };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
-  // Instagram, Twitter and Kaggle are also supported in the links!
-  // To customize icons and social links, tweak src/components/SocialMedia
-  display: true // Set true to display this section, defaults to false
-};
+    github: "https://github.com/diannevip321",
+    linkedin: "https://www.linkedin.com/in/dianne-vincent02/",
+    gmail: "dianne.vincent@stonybrook.edu",
+    display: true // Set true to display this section, defaults to false
+  };
+  
 
 // Skills Section
 
 const skillsSection = {
-  title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
-  skills: [
-    emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
-    ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
-    emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
-    )
-  ],
-
-  /* Make Sure to include correct Font Awesome Classname to view your icon
-https://fontawesome.com/icons?d=gallery */
-
-  softwareSkills: [
-    {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
-    },
-    {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
-    },
-    {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
-    },
-    {
-      skillName: "JavaScript",
-      fontAwesomeClassname: "fab fa-js"
-    },
-    {
-      skillName: "reactjs",
-      fontAwesomeClassname: "fab fa-react"
-    },
-    {
-      skillName: "nodejs",
-      fontAwesomeClassname: "fab fa-node"
-    },
-    {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
-    },
-    {
-      skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
-    },
-    {
-      skillName: "sql-database",
-      fontAwesomeClassname: "fas fa-database"
-    },
-    {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
-    },
-    {
-      skillName: "python",
-      fontAwesomeClassname: "fab fa-python"
-    },
-    {
-      skillName: "docker",
-      fontAwesomeClassname: "fab fa-docker"
-    }
-  ],
-  display: true // Set false to hide this section, defaults to true
-};
+    title: "What I do",
+    subTitle: "CREATIVE DEVELOPER WHO BUILDS SCALABLE SOLUTIONS FOR REAL-WORLD CHALLENGES",
+    skills: [
+      emoji(
+        "⚡ Develop responsive and user-friendly frontends with React and Material UI"
+      ),
+      emoji("⚡ Create secure and scalable backend services with Go and Node.js"),
+      emoji(
+        "⚡ Integrate advanced machine learning models to solve complex problems"
+      )
+    ],
+  
+    softwareSkills: [
+      { skillName: "Python", fontAwesomeClassname: "fab fa-python" },
+      { skillName: "JavaScript", fontAwesomeClassname: "fab fa-js" },
+      { skillName: "React", fontAwesomeClassname: "fab fa-react" },
+      { skillName: "Node.js", fontAwesomeClassname: "fab fa-node" },
+      { skillName: "Go", fontAwesomeClassname: "fab fa-golang" },
+      { skillName: "Docker", fontAwesomeClassname: "fab fa-docker" }
+    ],
+    display: true // Set false to hide this section, defaults to true
+  };
+  
 
 // Education Section
 
 const educationInfo = {
-  display: true, // Set false to hide this section, defaults to true
-  schools: [
-    {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
-      descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      ]
-    },
-    {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
-    }
-  ]
-};
+    display: true,
+    schools: [
+      {
+        schoolName: "Stony Brook University",
+        logo: require("./assets/images/stonyBrookLogo.png"),
+        subHeader: "Bachelor of Science in Computer Science",
+        duration: "August 2021 - May 2025",
+        desc: "Wise Honors Program: Women in Science and Engineering",
+        descBullets: [
+          "Courses: Software Engineering, Machine Learning, NLP, Computer Networks"
+        ]
+      }
+    ]
+  };
+  
 
 // Your top 3 proficient stacks/tech experience
 
 const techStack = {
-  viewSkillBars: true, //Set it to true to show Proficiency Section
-  experience: [
-    {
-      Stack: "Frontend/Design", //Insert stack or technology you have experience in
-      progressPercentage: "90%" //Insert relative proficiency in percentage
-    },
-    {
-      Stack: "Backend",
-      progressPercentage: "70%"
-    },
-    {
-      Stack: "Programming",
-      progressPercentage: "60%"
-    }
-  ],
-  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
-};
+    viewSkillBars: true, // Set it to true to show Proficiency Section
+    experience: [
+      {
+        Stack: "Backend Development", // Insert stack or technology you have experience in
+        progressPercentage: "85%" // Based on skills like React, Material UI, etc.
+      },
+      {
+        Stack: "Frontend logic",
+        progressPercentage: "75%" // Based on skills like Node.js, Go, etc.
+      },
+      {
+        Stack: "Machine Learning",
+        progressPercentage: "70%" // Based on skills like TensorFlow, PyTorch, etc.
+      },
+      {
+        Stack: "Programming",
+        progressPercentage: "90%" // Strong foundational programming skills
+      },
+      {
+        Stack: "Data Analytics",
+        progressPercentage: "80%" // Experience with Python libraries, SQL, etc.
+      }
+    ],
+    displayCodersrank: false // Set true to display CodersRank badges section
+  };
 
 // Work experience section
 
 const workExperiences = {
-  display: true, //Set it to true to show workExperiences Section
-  experience: [
-    {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      ]
-    },
-    {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    }
-  ]
-};
+    display: true,
+    experience: [
+      {
+        role: "Research Assistant",
+        company: "Stony Brook University",
+        companylogo: require("./assets/images/sbuCS.png"),
+        date: "August 2024 – Present",
+        desc: "Conducting research on bias mitigation in AI models under Dr. Praveen Tripathi, leveraging frameworks like PyTorch and TensorFlow to ensure equitable AI outcomes."
+      },
+      {
+        role: "Classroom Technology Assistant - Data Analytics",
+        company: "Division of Information Technology, SBU",
+        companylogo: require("./assets/images/sbuMedicine.jpeg"),
+        date: "June 2023 – Present",
+        desc: "Developed Python-based data analytics scripts to streamline annual reporting and trained faculty on classroom IT systems."
+      },
+      {
+        role: "Teaching Assistant",
+        company: "Stony Brook University",
+        companylogo: require("./assets/images/stonyBrookLogo.png"),
+        date: "August 2023 – Present",
+        desc: "Taught lab sessions for Object-Oriented Programming (Java) and Data Science (Python)."
+      }
+    ]
+  };
+  
 
 /* Your Open Source Section to View Your Github Pinned Projects
 To know how to get github key look at readme.md */
@@ -212,35 +155,38 @@ const openSource = {
 // Some big projects you have worked on
 
 const bigProjects = {
-  title: "Big Projects",
-  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
-  projects: [
-    {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      footerLink: [
-        {
-          name: "Visit Website",
-          url: "http://saayahealth.com/"
-        }
-        //  you can add extra buttons here.
+    title: "Projects",
+    subtitle: "CREATING INNOVATIVE SOLUTIONS",
+    projects: [
+      {
+        projectName: "Decentralized File-Sharing Application",
+        projectDesc:
+          "Cross-platform blockchain-based desktop application for secure file sharing. Utilized IPFS for decentralized storage and Bitcoin for transactions, with React and TypeScript for the frontend and Go for the backend.",
+        footerLink: [
+        { name: "View on GitHub", url: "https://github.com/diannevip321" }
       ]
-    },
-    {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      footerLink: [
-        {
-          name: "Visit Website",
-          url: "http://nextu.se/"
-        }
-      ]
-    }
-  ],
-  display: true // Set false to hide this section, defaults to true
-};
+      },
+
+      {
+        projectName: "HappinessML",
+        projectDesc:
+          "Analyzed World Happiness Report data to predict happiness scores using Python and Scikit-Learn. Models like Linear Regression and Ridge Regression provided insights into socio-economic factors affecting happiness.",
+          footerLink: [
+            { name: "View on GitHub", url: "https://github.com/diannevip321" }
+          ]
+      },
+      {
+        projectName: "StackOverflow Clone",
+        projectDesc:
+          "Feature-rich web application replicating StackOverflow functionality. Developed with React, Material UI, Node.js, and MongoDB to support Q&A interactions and secure user authentication.",
+          footerLink: [
+            { name: "View on GitHub", url: "https://github.com/diannevip321" }
+          ]
+      }
+    ],
+    display: true
+  };
+  
 
 // Achievement Section
 // Include certificates, talks etc
@@ -300,7 +246,7 @@ const achievementSection = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Blogs Section
@@ -324,7 +270,7 @@ const blogSection = {
         "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Talks Sections
@@ -343,7 +289,7 @@ const talkSection = {
       event_url: "https://www.facebook.com/events/2339906106275053/"
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Podcast Section
@@ -356,7 +302,7 @@ const podcastSection = {
   podcast: [
     "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Resume Section
@@ -369,18 +315,17 @@ const resumeSection = {
 };
 
 const contactInfo = {
-  title: emoji("Contact Me ☎️"),
-  subtitle:
-    "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
-};
-
+    title: emoji("Contact Me ☎️"),
+    subtitle:
+      "Have a project or opportunity in mind? Let’s collaborate!",
+    number: "(631) 710-5599",
+    email_address: "dianne.vincent@stonybrook.edu"
+  };
 // Twitter Section
 
 const twitterDetails = {
   userName: "twitter", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
+  display: false // Set true to display this section, defaults to false
 };
 
 const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
